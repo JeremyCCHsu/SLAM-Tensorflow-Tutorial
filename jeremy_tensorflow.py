34a218f2ca804a58b424bdc4c4ad79535a213e38
JeremyCCHsu <jeremycchsu@gmail.com>
2015-12-09 21:37:39 +0800

    Update weight init

--- jeremy_tensorflow.py
+++ jeremy_tensorflow.py
@@ -114,12 +114,12 @@ def show_img_keypoint(img, label, truth=None):
 	plt.show()
 
 
-def weight_variable(shape):
-	w_initial = tf.truncated_normal(shape, stddev=0.1)
+def weight_variable(shape, std=0.0):
+	w_initial = tf.truncated_normal(shape, stddev=std)
 	return tf.Variable(w_initial)
 
 def bias_variable(shape):
-	b_initial = tf.constant(0.1, shape=shape)
+	b_initial = tf.constant(0.0, shape=shape)
 	return tf.Variable(b_initial)
 
 def conv2d(x, W):
@@ -134,9 +134,6 @@ def max_pool_2x2(x):
 
 
 if __name__ == '__main__':
-	# x_data, y_data = load(test=False, valid=0.2)
-	# print x_data
-	# print y_data
 	dataset = load(test=False, valid=0.2)
 
 	iDim = 96*96
@@ -153,54 +150,20 @@ if __name__ == '__main__':
 
 	W  = tf.Variable(
 		tf.truncated_normal(
-			[iDim, hDim], stddev=0.1))
+			[iDim, hDim], stddev=1.0/iDim))
 	b  = tf.Variable(tf.constant(0.1, shape=[hDim]))
 	y1 = tf.nn.relu(tf.matmul(x, W) + b)
 
 	W2 = tf.Variable(
 		tf.truncated_normal(
-			[hDim, oDim], stddev=0.1))
+			[hDim, oDim], stddev=1.0/hDim))
 	b2 = tf.Variable(tf.constant(0.1, shape=[oDim]))
 	y  = tf.matmul(y1, W2) + b2   # tf.nn.softmax(
 
+	loss = tf.reduce_mean(
+			tf.reduce_sum(
+			tf.square(y - y_), 1))
 
-	# W_conv1 = weight_variable([3, 3, 1, 32])
-	# b_conv1 = bias_variable([32])
-	# x_image = tf.reshape(x, [-1, 96, 96, 1])
-
-	# h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-	# h_pool1 = max_pool_2x2(h_conv1)
-
-	# W_conv2 = weight_variable([2, 2, 32, 64])
-	# b_conv2 = bias_variable([64])
-
-	# h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-	# h_pool2 = max_pool_2x2(h_conv2)
-
-	# W_conv3 = weight_variable([2, 2, 64, 128])
-	# b_conv3 = bias_variable([128])
-
-	# h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-	# h_pool3 = max_pool_2x2(h_conv3)
-
-	# W_dense4 = weight_variable([11 * 11 * 128, 500])
-	# b_dense4 = bias_variable([500])
-
-	# h_pool3_flat = tf.reshape(h_pool3, [-1, 11 * 11 * 128])
-	# h_dense4 = tf.nn.relu(tf.matmul(h_pool3_flat, W_dense4) + b_dense4)
-
-	# W_dense5 = weight_variable([500, 500])
-	# b_dense5 = bias_variable([500])
-
-	# h_dense5 = tf.nn.relu(tf.matmul(h_dense4, W_dense5) + b_dense5)
-
-	# W_dense6 = weight_variable([500, 30])
-	# b_dense6 = bias_variable([30])
-
-	# y = tf.matmul(h_dense5, W_dense6) + b_dense6
-
-	err = y - y_
-	loss = tf.reduce_mean(tf.reduce_sum(err * err, 1))
 	train_step = tf.train.AdamOptimizer(1e-2).minimize(loss)
 
 	init = tf.initialize_all_variables()
@@ -210,10 +173,8 @@ if __name__ == '__main__':
 	loss_train_record = list() # np.zeros(n_epoch)
 	loss_valid_record = list() # np.zeros(n_epoch)
 	start_time = time.gmtime()
-	# for i in range(n_epoch):
 	while dataset.train.epochs_completed < n_epoch:
 		batch = dataset.train.next_batch(BATCH_SIZE)
-	# 	feeds = {x: batch[0], y_: batch[1]}
 		train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 		loss_train = sess.run(loss, feed_dict={x: batch[0], y_: batch[1]})
 		loss_valid = sess.run(loss, feed_dict={x: dataset.valid.images, y_: dataset.valid.labels})
@@ -228,7 +189,6 @@ if __name__ == '__main__':
 	print time.strftime('%H:%M:%S', start_time)
 	print time.strftime('%H:%M:%S', end_time)
 
-	# Show an example of comparison
 	i = 0
 	img = dataset.valid.images[i]
 	lab_y = dataset.valid.labels[i]
@@ -238,5 +198,3 @@ if __name__ == '__main__':
 	plt.plot(loss_train_record)
 	plt.plot(loss_valid_record, c='r')
 	plt.show()
-
-	# correct_prediction
